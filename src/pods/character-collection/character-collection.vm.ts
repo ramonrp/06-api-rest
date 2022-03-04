6aebe1226a3c6185750457950860e71ee039b7cf
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-04 11:20:36 +0100

    add view model character entity

---
+++ src/pods/character-collection/character-collection.vm.ts
@@ -0,0 +1,12 @@
+interface CharacterEntity {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  image: string;
+  url: string;
+}
+
+export { CharacterEntity };
