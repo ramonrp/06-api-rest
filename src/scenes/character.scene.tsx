59851cce834c5be07b0b5e0cbe4c9fe1cd73a881
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-04 13:54:32 +0100

    fix characterScene name

--- src/scenes/character.scene.tsx
+++ src/scenes/character.scene.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { AppLayout } from 'layouts';
 import { CharacterContainer } from 'pods/character';
-export const CharacterCollectionScene = () => (
+export const CharacterScene = () => (
   <AppLayout>
     <CharacterContainer />
   </AppLayout>
