89f3cbe20fd5a493a040811d863d72446bd56fa0
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-04 13:31:14 +0100

    update character collection component to use Character Card

--- src/pods/character-collection/character-collection.component.tsx
+++ src/pods/character-collection/character-collection.component.tsx
@@ -1,7 +1,31 @@
 import * as React from 'react';
+import { CharacterEntity } from './character-collection.vm';
+import { CharacterCard } from './components/character-card';
+import * as classes from './character-collection.styles';
 
-const CharacterCollection = () => {
-  return <h1>Character Collection</h1>;
+interface Props {
+  characterCollection: CharacterEntity[];
+  onDetail: (id: string) => void;
+}
+
+const CharacterCollection: React.FunctionComponent<Props> = (props) => {
+  const { characterCollection, onDetail } = props;
+  console.log(characterCollection);
+  return (
+    <div className={classes.root}>
+      <ul className={classes.list}>
+        {characterCollection?.map((character) => {
+          return (
+            <CharacterCard
+              key={character.id}
+              character={character}
+              onDetail={onDetail}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export { CharacterCollection };
