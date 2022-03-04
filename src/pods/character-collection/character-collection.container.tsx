73ac8747faf73ef57a2b5bc02275f50ac5ad3096
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-04 13:31:47 +0100

    get characters from api and create handleDetail func

--- src/pods/character-collection/character-collection.container.tsx
+++ src/pods/character-collection/character-collection.container.tsx
@@ -1,7 +1,27 @@
 import * as React from 'react';
 import { CharacterCollection } from './character-collection.component';
-const CharacterCollectionContainer = () => {
-  return <CharacterCollection />;
+import { CharacterEntity } from './character-collection.vm';
+import { getCharacterCollection } from './api';
+import { linkRoutes } from 'core/router';
+import { useHistory } from 'react-router-dom';
+import { mapCharacterListFromApiToVM } from './character-collection.mapper';
+const CharacterCollectionContainer: React.FC = () => {
+  const [characters, setCharacters] = React.useState<CharacterEntity[]>();
+  React.useEffect(() => {
+    getCharacterCollection()
+      .then(mapCharacterListFromApiToVM)
+      .then(setCharacters);
+  });
+  const history = useHistory();
+  const handleDetail = (id: string) => {
+    history.push(linkRoutes.characterDetail(id));
+  };
+  return (
+    <CharacterCollection
+      characterCollection={characters}
+      onDetail={handleDetail}
+    />
+  );
 };
 
 export { CharacterCollectionContainer };
