898c071d7104ccf55c12a740d9d06afea35b76e8
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-04 13:52:03 +0100

    create character scene

---
+++ src/scenes/character.scene.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { AppLayout } from 'layouts';
+import { CharacterContainer } from 'pods/character';
+export const CharacterCollectionScene = () => (
+  <AppLayout>
+    <CharacterContainer />
+  </AppLayout>
+);
