3f3c5a6296f87c856b9dd26a4ef4923d9f798cbd
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-04 12:01:54 +0100

    change routes to add character collection scene

--- src/core/router/router.component.tsx
+++ src/core/router/router.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import { switchRoutes } from './routes';
-import { HotelCollectionScene, HotelScene } from 'scenes';
+import { CharacterCollectionScene } from 'scenes';
 
 export const RouterComponent: React.FunctionComponent = () => {
   return (
@@ -9,18 +9,8 @@ export const RouterComponent: React.FunctionComponent = () => {
       <Switch>
         <Route
           exact={true}
-          path={[switchRoutes.root, switchRoutes.hotelCollection]}
-          component={HotelCollectionScene}
-        />
-        <Route
-          exact={true}
-          path={switchRoutes.createHotel}
-          component={HotelScene}
-        />
-        <Route
-          exact={true}
-          path={switchRoutes.editHotel}
-          component={HotelScene}
+          path={[switchRoutes.root, switchRoutes.characterCollection]}
+          component={CharacterCollectionScene}
         />
       </Switch>
     </HashRouter>
--- src/core/router/routes.ts
+++ src/core/router/routes.ts
@@ -2,25 +2,23 @@ import { generatePath } from 'react-router';
 
 interface SwitchRoutes {
   root: string;
-  hotelCollection: string;
-  createHotel: string;
-  editHotel: string;
+  characterCollection: string;
+  characterDetail: string;
 }
 
 export const switchRoutes: SwitchRoutes = {
   root: '/',
-  hotelCollection: '/hotels',
-  createHotel: '/hotels/create',
-  editHotel: '/hotels/:id',
+  characterCollection: '/characters',
+  characterDetail: '/characters/:characterId',
 };
 
 type NavigationFunction = (id: string) => string;
 
-interface LinkRoutes extends Omit<SwitchRoutes, 'editHotel'> {
-  editHotel: NavigationFunction;
+interface LinkRoutes extends Omit<SwitchRoutes, 'characterDetail'> {
+  characterDetail: NavigationFunction;
 }
 
 export const linkRoutes: LinkRoutes = {
   ...switchRoutes,
-  editHotel: (id) => generatePath(switchRoutes.editHotel, { id }),
+  characterDetail: (id) => generatePath(switchRoutes.characterDetail, { id }),
 };
