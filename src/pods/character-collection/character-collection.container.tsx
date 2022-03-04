337812e9aa5423119657ec15a3b5f5e0ffe534c4
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-04 11:57:43 +0100

    create characterCollection container

---
+++ src/pods/character-collection/character-collection.container.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { CharacterCollection } from './character-collection.component';
+const CharacterCollectionContainer = () => {
+  return <CharacterCollection />;
+};
+
+export { CharacterCollectionContainer };
