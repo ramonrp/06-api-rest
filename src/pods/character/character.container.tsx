3b33c538c9b793e792756a64e11a7fc90aa76474
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-07 13:21:49 +0100

    add handleSave

--- src/pods/character/character.container.tsx
+++ src/pods/character/character.container.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { useCharacter } from './character.hooks';
 import { Character } from './character.component';
+import { saveSentences } from './api';
+import { useHistory } from 'react-router-dom';
 interface Props {
   characterId: string;
 }
@@ -8,10 +10,19 @@ interface Props {
 const CharacterContainer: React.FC<Props> = (props) => {
   const { characterId } = props;
   const { character } = useCharacter(characterId);
+  const history = useHistory();
+  const handleSave = async (bestSentences: string) => {
+    const success = await saveSentences(characterId, bestSentences);
+    if (success) {
+      history.goBack();
+    } else {
+      alert('Error on save sentences');
+    }
+  };
   if (!character) {
     return <div>Loading</div>;
   }
-  return <Character character={character} />;
+  return <Character character={character} onSave={handleSave} />;
 };
 
 export { CharacterContainer };
