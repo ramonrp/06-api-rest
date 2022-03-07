2c6d708065df0e5b3783466a24a4aef9125e43ac
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-07 13:22:05 +0100

    add onSave prop and handleSubmit

--- src/pods/character/character.component.tsx
+++ src/pods/character/character.component.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import { CharacterEntity } from './character.vm';
 import * as classes from './character.component.styles';
+import Button from '@material-ui/core/Button';
 interface Props {
   character: CharacterEntity;
+  onSave: (bestSentences: string) => void;
 }
 
 const Character: React.FunctionComponent<Props> = (props) => {
-  const { character } = props;
+  const { character, onSave } = props;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const bestSentences = e.target.elements.bestSentences.value;
+    onSave(bestSentences);
+  };
   return (
     <div className={classes.wrapper}>
       <div>
@@ -21,6 +29,17 @@ const Character: React.FunctionComponent<Props> = (props) => {
         <div>Specie:{character.species}</div>
         <div>Status:{character.status}</div>
         <div>Gender:{character.gender}</div>
+        <form onSubmit={handleSubmit}>
+          <p> Best Sentences: </p>
+          <textarea
+            id="bestSentences"
+            className={classes.textArea}
+            defaultValue={character.bestSentences}
+          ></textarea>
+          <Button type="submit" color="primary">
+            Save new best Sentences
+          </Button>
+        </form>
       </div>
     </div>
   );
