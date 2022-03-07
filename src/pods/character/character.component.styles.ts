8b052b44e6c475bc1e8895987a2017663fadd581
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-07 10:42:36 +0100

    add info and styles to character detail

---
+++ src/pods/character/character.component.styles.ts
@@ -0,0 +1,16 @@
+import { css } from 'emotion';
+
+export const wrapper = css`
+  width: 100%;
+  max-width: 450px;
+`;
+
+export const info = css`
+  text-align: center;
+  font-size: 1.25rem;
+`;
+
+export const img = css`
+  width: 100%;
+  display: block;
+`;
--- src/pods/character/character.component.tsx
+++ src/pods/character/character.component.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { CharacterEntity } from './character.vm';
-
+import * as classes from './character.component.styles';
 interface Props {
   character: CharacterEntity;
 }
@@ -8,17 +8,20 @@ interface Props {
 const Character: React.FunctionComponent<Props> = (props) => {
   const { character } = props;
   return (
-    <div>
-      {
-        <div>
-          <div>
-            <img src={character.image} alt={character.name}></img>
-          </div>
-          <div>{character.name}</div>
-          <div>{character.species}</div>
-          <div>{character.status}</div>
-        </div>
-      }
+    <div className={classes.wrapper}>
+      <div>
+        <img
+          src={character.image}
+          alt={character.name}
+          className={classes.img}
+        ></img>
+      </div>
+      <div className={classes.info}>
+        <div>Name:{character.name}</div>
+        <div>Character:{character.species}</div>
+        <div>Status:{character.status}</div>
+        <div>Gender:{character.gender}</div>
+      </div>
     </div>
   );
 };
