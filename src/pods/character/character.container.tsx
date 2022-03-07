7ef7a7ceebc26516584541f7bd5e0b6e2901b379
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-03-07 09:04:31 +0100

    add getCharacter from api to character container

--- src/pods/character/character.container.tsx
+++ src/pods/character/character.container.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import { getCharacter } from './api';
+import { mapCharacterFromApiToVM } from './character.mapper';
+import { CharacterEntity } from './character.vm';
 
 const CharacterContainer: React.FC = () => {
-  return <h1>Character Container</h1>;
+  const [character, setCharacter] = React.useState<CharacterEntity>();
+  React.useEffect(() => {
+    getCharacter().then(mapCharacterFromApiToVM).then(setCharacter);
+  }, []);
+  return <h1>{character?.name}</h1>;
 };
 
 export { CharacterContainer };
